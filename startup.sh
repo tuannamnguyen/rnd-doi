20b98ab83a3a82869ec46344fd71066c8377608f
tuannamnguyen <tuannamnguyen290602@gmail.com>
2023-10-04 10:32:19 +0700

    update script

--- startup.sh
+++ startup.sh
@@ -1,5 +1,7 @@
 sudo apt update -y && sudo apt upgrade -y
 curl -sSL https://install.python-poetry.org | python3 -
 poetry config virtualenvs.in-project true
-poetry install && poetry shell
+poetry install
+source .venv/bin/activate
 pre-commit install
+pre-commit run --all-files
