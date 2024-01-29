9c2f7c23c5e72d88e3366d4cf71b0f8acf5ce8c3
namnt134 <namnt134@fpt.com>
2024-01-29 13:58:34 +0700

    update script

--- startup.sh
+++ startup.sh
@@ -2,7 +2,7 @@
 # Nếu k chạy được script thì chạy lệnh sudo chmod +x ./*.sh
 
 sudo apt update -y && sudo apt upgrade -y
-curl -sSL https://install.python-poetry.org | python3 -
+pipx install poetry
 poetry config virtualenvs.in-project true
 poetry install
 source .venv/bin/activate
